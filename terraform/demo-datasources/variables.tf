a1ef59f2cdca7626da7f6c8e3d59113ee592c6b2
Ng Yew Kong <ngyewkong1996@gmail.com>
2024-12-02 16:04:56 +0800

    add datasources impl

---
+++ terraform/demo-datasources/variables.tf
@@ -0,0 +1,34 @@
+# leaving blank for AWS_ACCESS_KEY & AWS_SECRET_KEY will prompt in terminal for input during runtime
+variable "AWS_ACCESS_KEY" {
+  sensitive = true
+}
+
+variable "AWS_SECRET_KEY" {
+  sensitive = true
+}
+
+variable "AWS_REGION" {
+  default = "us-east-2"
+}
+
+variable "INSTANCE_COUNT" {
+  default = 3
+}
+
+variable "Security_Group" {
+  type    = list(string)
+  default = ["sg-0c7de8c9df43291a2"]
+}
+
+variable "AMI" {
+  type = map(string)
+  default = {
+    us-west-1 = "ami-0454207e5367abf01"
+    us-west-2 = "ami-0688ba7eeeeefe3cd"
+  }
+
+}
+
+variable "INSTANCE_NAME" {
+  default = "ubuntu"
+}
